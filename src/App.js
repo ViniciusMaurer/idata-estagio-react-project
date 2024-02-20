80464ecd04c001bc1a1ff41cad97752c8f478d8a
Vinícius Maurer <viniciusmaurer8@gmail.com>
2024-02-19 23:51:02 -0300

    Integração do AG-GRID com os dados do banco

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from './Header';
-import Table from './Table';
 import Conexao from './Conexao';
 
 function App() {
+  
   return (
     <div>
       <Header></Header>
-      <Table></Table>
       <Conexao></Conexao>
     </div>
   );
--- src/Conexao.js
+++ src/Conexao.js
@@ -1,5 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
 
 
 class Conexao extends Component {
@@ -7,18 +11,31 @@ class Conexao extends Component {
     recruitment: []
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('https://localhost:7232/api/v1/idata').then(response => {
-      console.log(response.data)
-      const recruitment = response.data
-      this.setState({ recruitment });
-    })
+      console.log(response.data);
+      this.setState({ recruitment: response.data });
+    });
   }
 
-  render(){
+  render() {
+    const columnDefs = [
+      { headerName: "ID", field: "id" },
+      { headerName: "Exportador", field: "exportador" },
+      { headerName: "Importador", field: "importador" },
+      { headerName: "Data de Embarque", field: "dataEmbarque" },
+      { headerName: "Previsão de Embarque", field: "previsaoDeEmbarque" },
+      { headerName: "Data de Chegada", field: "dataChegada" },
+      { headerName: "Previsão de Chegada", field: "previsaoDeChegada" },
+    ];
+
     return (
-      <div>
-        <h1>Teste de Conexao - {this.state.recruitment.length}</h1>
+      <div className="ag-theme-alpine" style={{ height: '800px', width: '100%' }}>
+        <AgGridReact
+          columnDefs={columnDefs}
+          rowData={this.state.recruitment}
+          pagination={true}
+        />
       </div>
     );
   }
